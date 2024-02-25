6d335b4138956dd6449af8ea4db8349a823792f2
Eliot Robson <eliot.robson24@gmail.com>
2024-02-24 23:17:08 -0600

    Added new CI

--- tests/test_speed.py
+++ tests/test_speed.py
@@ -1,22 +1,21 @@
+import random
+
+import chevron
 
 import chevron2
-import chevron
-import random
+
 
 def test_large() -> None:
     n = 10_000
 
-    names = {
-        f"thing{i}": i
-        for i in range(n)
-    }
+    names = {f"thing{i}": i for i in range(n)}
 
     template = "".join(r"{{" + name + "}}" for name in names.keys())
 
-
     import time
+
     start = time.perf_counter()
     chevron.render(template, names)
     end = time.perf_counter()
-    print(end-start)
-    assert False
+    print(end - start)
+    # assert False

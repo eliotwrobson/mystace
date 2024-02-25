c0f12625daff092a439beac8de30534057fc9920
Eliot Robson <eliot.robson24@gmail.com>
2024-02-25 00:18:40 -0600

    Benchmark is working

--- tests/test_speed.py
+++ tests/test_speed.py
@@ -1,21 +1,28 @@
 # import random
 
+import typing as t
+
 import chevron
+import pytest
+from typing_extensions import assert_never
 
-# import chevron2
+from chevron2.renderer import render
+
+RenderFunctionT = t.Literal["chevron2", "chevron"]
 
 
-def test_large() -> None:
-    n = 10_000
+@pytest.mark.parametrize("render_function_name", t.get_args(RenderFunctionT))
+def test_large(render_function_name: RenderFunctionT, benchmark) -> None:
+    n = 50_000
+
+    if render_function_name == "chevron2":
+        render_function = render
+    elif render_function_name == "chevron":
+        render_function = chevron.render
+    else:
+        assert_never(render_function_name)
 
     names = {f"thing{i}": i for i in range(n)}
-
     template = "".join(r"{{" + name + "}}" for name in names.keys())
 
-    import time
-
-    start = time.perf_counter()
-    chevron.render(template, names)
-    end = time.perf_counter()
-    print(end - start)
-    # assert False
+    benchmark(render_function, template, names)

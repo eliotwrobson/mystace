46464202e6d8829ebb4d5f3ed4508ef6808aaf63
Eliot Robson <eliot.robson24@gmail.com>
2024-02-25 23:02:06 -0600

    Making progress on tokenized

--- tests/test_tokenize.py
+++ tests/test_tokenize.py
@@ -3,6 +3,7 @@ from chevron2 import tokenize
 
 def test_split_into_sub_tokens() -> None:
     thing = r"A{{#x}}B{{#x}}{{.}}{{/x}}C{{/x}}D"
-    res = tokenize(thing)
+    data = {"x": ["z", "x"]}
+    res = tokenize(thing, data)
     print(res)
     assert False

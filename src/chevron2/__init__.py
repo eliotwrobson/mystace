546cd23881e6d1832f1b38590822155bcf2cbd33
Eliot Robson <eliot.robson24@gmail.com>
2024-03-16 22:33:08 -0500

    Fixed CI

--- src/chevron2/__init__.py
+++ src/chevron2/__init__.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
 Mustache v1.4 implementation with lambdas.
 
--- src/chevron2/exceptions.py
+++ src/chevron2/exceptions.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
 class Chevron2Error(Exception):
     """
     Chevron2 base exception. Not raised.
